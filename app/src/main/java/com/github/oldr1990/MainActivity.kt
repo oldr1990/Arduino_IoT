6a495031b6e010e1ca1ae46376f7a6c7cb34a42d
oldr1990 <sashik4281@gmail.com>
2021-07-13 22:16:50 +0300

    Initial commit

--- app/src/main/java/com/github/oldr1990/MainActivity.kt
+++ app/src/main/java/com/github/oldr1990/MainActivity.kt
@@ -24,15 +24,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ArduinoIoTTheme {
-        Greeting("Android")
-    }
-}
